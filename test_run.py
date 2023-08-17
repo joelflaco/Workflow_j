4b57a705cbf008691ddd7b3746b3a8490f411761
joelflaco <115979421+joelflaco@users.noreply.github.com>
2023-08-17 15:35:42 +0200

    test_run.py

--- test_run.py
+++ test_run.py
@@ -1,17 +1,13 @@
 import unittest
-
 from run import minus
 
-
 class TestMinus(unittest.TestCase):
-
     def test_minus_function(self):
-        self.assertEqual(minus(1, 2), 3)
-        self.assertEqual(minus(4, 7), 11)
+        self.assertEqual(minus(3, 1), 2)  
+        self.assertEqual(minus(7, 4), 3)  
 
     def test_minus_function_with_floats(self):
-        self.assertAlmostEqual(minus(2.1, 5.2), 7.3)
-
+        self.assertAlmostEqual(minus(5.2, 2.1), 3.1) 
 
 if __name__ == "__main__":
     unittest.main()
